d9a183310dc8b8f111cce1e3b70e758375442cea
jinyowo <hjin421@gmail.com>
2015-05-07 22:07:41 +0900

    Main UI
    지구위에 위도, 경도에 따라 자동으로 object를 만들어 위치를 표시해줌

---
+++ MainUI/Assets/EarthScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider))]
+
+public class EarthScript : MonoBehaviour
+{
+    public int Speed;		//rotate speed
+    public float rotatePosY; //y axis rotate degree
+    public float rotatePosX; //x axis rotate degree
+
+    void Start()
+    {
+        Speed = 10;
+        rotatePosY = 25;
+        rotatePosX = 0;
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.up, Speed * Time.deltaTime * 5, Space.Self);
+            rotatePosY += Speed * Time.deltaTime * 5;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.down, Speed * Time.deltaTime * 5, Space.Self);
+            rotatePosY -= Speed * Time.deltaTime * 5;
+        }
+  
+    }
+
+
+    void OnMouseDown()
+    {
+
+    }
+
+}
+
+
+
---
+++ MainUI/Assets/LatLng.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LatLng : MonoBehaviour
+{
+
+    public string ID;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnMouseDown()
+    {
+        Debug.Log("mouse click : " + ID);
+    }
+    void OnMouseOver()
+    {
+        //표지판 그리기
+    }
+    public void SetPosition()
+    {
+        ID = CreateCube.panoID;
+
+        transform.Rotate(CreateCube._rotation);
+        transform.Translate(CreateCube._translate);
+        Debug.Log("move object : " + ID);
+    }
+
+}
