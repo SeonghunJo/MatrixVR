fd869370425cddca34b5328af2c6783087b1e6fa
jsh4051 <jsh4051@gmail.com>
2015-05-11 04:02:46 +0900

    Loading Screen 구현
    Unity Pro 에서만 사용 가능한 함수를 포함해 수정해야하는 이슈가 있음

--- MainUI/Assets/Prefab/LoadingScreen.cs
+++ MainUI/Assets/Prefab/LoadingScreen.cs
@@ -3,6 +3,51 @@ using System.Collections;
 
 public class LoadingScreen : MonoBehaviour {
 
+	public string levelToLoad;
+
+	public GameObject background;
+	public GameObject text;
+	public GameObject progressBar;
+
+	private int loadProgress = 0;
+
+	// Use this for initialization
+	void Start () {
+		background.SetActive(false);
+		text.SetActive(false);
+		progressBar.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("space")) {
+			StartCoroutine(DisplayLoadingScreen(levelToLoad));
+		}
+	}
+
+	IEnumerator DisplayLoadingScreen(string level) {
+		background.SetActive(true);
+		text.SetActive(true);
+		progressBar.SetActive(true);
+
+		progressBar.transform.localScale = new Vector3(loadProgress, progressBar.transform.localScale.y, progressBar.transform.localScale.z);
+
+		text.guiText.text = "Loading Progress " + loadProgress + "%";
+
+		AsyncOperation async = Application.LoadLevelAsync(levelToLoad);
+
+		while (!async.isDone) {
+			loadProgress = (int)(async.progress * 100);
+			text.guiText.text = "Loading Progress " + loadProgress + "%";
+			progressBar.transform.localScale = new Vector3(loadProgress, progressBar.transform.localScale.y, progressBar.transform.localScale.z);
+			
+			yield return null;
+		}
+	}
+}
+/*
+public class LoadingScreen : MonoBehaviour {
+
 	public Texture2D texture;
 	static LoadingScreen _instance;
 
@@ -68,3 +113,4 @@ public class LoadingScreen : MonoBehaviour {
 	
 	}
 }
+*/
