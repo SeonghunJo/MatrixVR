7430d184fce3e45cf5297fa1a46eed3aada5c3d2
hyunseonYang <vpqm@naver.com>
2015-05-19 22:00:39 +0900

    Guide
    제스처 가이드

---
+++ MainUI/Assets/Scripts/MainUI/Guide.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Guide : MonoBehaviour {
+	float timeCounter =0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeCounter = Time.time;
+		
+		if (timeCounter > 15) { 
+			gameObject.transform.localScale -= new Vector3(Time.deltaTime*35F,Time.deltaTime*30F,0);
+			if(gameObject.transform.localScale.x<=1F |gameObject.transform.localScale.y<=1F)
+				Destroy(gameObject);
+		}
+	}
+}
