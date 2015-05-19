31ec668fe8028aebbb5fcec8e72488359b667670
Hongjoon Jeon <kyrate@naver.com>
2015-05-19 21:32:37 +0900

    립모션동작수정, 메인크릭버튼추가, 인터페이스 조정
    1.GestureController 립모션 동작수정
    2.MainMenuScript 메인클릭버튼 스크립트 오브젝트 추가
    3.인터페이스 조정

---
+++ MainUI/Assets/Scripts/LeapMotion/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	void start()
+	{
+		this.renderer.enabled = false;
+	}
+	public void Clicked()
+	{
+		Application.LoadLevel ("SceneEarth");
+	}
+	public void Pointed()
+	{
+		this.renderer.enabled = true;
+	}
+	public void PointedOut()
+	{
+		this.renderer.enabled = false;
+	}
+}
