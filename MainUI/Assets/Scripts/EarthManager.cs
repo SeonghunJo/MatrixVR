ce5877a7aa491b54771ab50daef06fe0d34930a2
hyunseonYang <vpqm@naver.com>
2015-05-20 20:34:04 +0900

    손 모델 , 가이드 화면 수정
    지구화면 손모델, 가이드 화면 수정

---
+++ MainUI/Assets/Scripts/EarthManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EarthManager {
+	private static EarthManager _instance;
+	public static EarthManager Instance
+	{
+		get
+		{
+			if(_instance == null)
+			{
+				_instance = new EarthManager();
+			}
+			return _instance;
+		}
+	}
+	public int startGuide;
+
+}
