c655a2b7d893f36b0a586d15024a16b1e4b549f3
Jun hong joon <kyarate@naver.com>
2015-05-27 16:26:19 +0900

    마우스로 여행지 선택되게! 하는법 아래참조
    MainCamera 활성화하고!
    LEapmotion카메라 비활성화하면 됩니다!
    
    마우스 가리키는 여행지가 파란색으로 바끼고 클릭하면 선택된다!
    아직 스트릿뷰화면에선 적용안해놨음... 지구에서만 됩니다

---
+++ MainUI/Assets/Scripts/LeapMotion/MouseController.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseController : MonoBehaviour {
+	StreetviewPoint StreetView_Pointed = null;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit = new RaycastHit();
+	
+		Ray r= Camera.main.ScreenPointToRay(Input.mousePosition);
+
+		Debug.DrawRay(r.origin, r.direction * 1000, Color.red);
+		
+
+		
+		if (Physics.Raycast(r, out hit, Mathf.Infinity))
+		{
+			if(hit.collider != null)
+			{
+				if(hit.collider.gameObject.tag == "StreetviewPoint")
+				{
+					StreetView_Pointed = hit.collider.gameObject.GetComponent<StreetviewPoint>();
+					if(StreetView_Pointed != null)
+					{
+						// TODO : Mouse Enter (SHJO)
+						StreetView_Pointed.gameObject.renderer.material.color=Color.blue;
+						
+					}
+
+				}
+				else
+				{
+					StreetView_Pointed.gameObject.renderer.material.color=Color.red;
+					StreetView_Pointed = null;
+				}
+			}
+		}
+		else
+		{
+			// TODO : Mouse Exit (SHJO)
+			if(StreetView_Pointed != null)
+			{
+				StreetView_Pointed.gameObject.renderer.material.color=Color.red;
+				StreetView_Pointed = null;
+			}
+		}
+
+		if (Input.GetMouseButtonDown (0)) {
+
+			if(StreetView_Pointed != null)
+			{
+				
+				Application.LoadLevel("StreetViewer");
+			}
+
+		}
+
+
+
+
+
+	
+	}
+}
